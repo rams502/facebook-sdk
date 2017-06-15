32789fdde0af6d7b0a00edc55ccaca800f270f0c
Ram <tech@janasenaparty.org>
2017-06-15 08:31:53 +0530

    push from local

--- examples/flask/config.py
+++ examples/flask/config.py
@@ -6,6 +6,7 @@ DEBUG = True
 SECRET_KEY = 'keep_it_like_a_secret'
 # here the sample change....
 ## second commit 
+###
 # Database details
 SQLALCHEMY_DATABASE_URI = '{0}{1}'.format('sqlite:///',
                                           path.join(BASE_DIRECTORY, 'app.db'))
